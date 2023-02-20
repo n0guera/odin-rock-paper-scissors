184ee1d329a2a3cc2d19d33d9bcc565dbf991e91
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-02-20 19:41:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function playerSelection() {
+    let selection = prompt('Rock, Paper, Scissors!');
+    selection;
+}
+playerSelection();
+
 function getComputerChoice() {
     const game = ['Rock', 'Paper', 'Scissors'];
     const random = game[Math.floor(Math.random() * game.length)];
